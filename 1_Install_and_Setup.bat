31a8bf1707346913ce941e232a24c55054802478
Sri Harsha Kota <sri@wetransform.it>
2026-06-30 07:38:49 -0400

    Add virtual environment creation to batch files

--- 1_Install_and_Setup.bat
+++ 1_Install_and_Setup.bat
@@ -1,7 +1,14 @@
 @echo off
+echo Creating Python Virtual Environment...
+python -m venv venv
+
+echo.
 echo Installing requirements...
+call venv\Scripts\activate.bat
 pip install -r requirements.txt
+
 echo.
 echo Starting Setup Wizard...
 python setup.py
+pause
 pause
--- 2_Run_Bot.bat
+++ 2_Run_Bot.bat
@@ -1,4 +1,5 @@
 @echo off
 echo Starting Transfer Bot...
+call venv\Scripts\activate.bat
 python bot.py
 pause
